ac2ea3a70b7ef56233cd478b6a43fda258635d07
Anup-1827 <anupalone.off@gmail.com>
2024-07-04 11:21:57 +0530

    Challenge Added

---
+++ src/_4_Challenge.ts
@@ -0,0 +1,43 @@
+enum Status{
+    Done = "done",
+    InProgress = "in-progress",
+    Todo = "todo"
+
+}
+
+interface ToDoItem{
+    id: number;
+    title: string;
+    status: Status;
+    completedOn ?: Date
+}
+
+
+
+const todoItems: ToDoItem[] = [
+    { id: 1, title: "Learn HTML", status: Status.Done, completedOn: new Date("2021-09-11") },
+    { id: 2, title: "Learn TypeScript", status: Status.InProgress },
+    { id: 3, title: "Write the best app in the world", status: Status.Todo },
+]
+
+function addTodoItem(todo:string): ToDoItem {
+    const id = getNextId(todoItems)
+
+    const newTodo = {
+        id,
+        title: todo,
+        status: Status.Todo,
+    }
+
+    todoItems.push(newTodo)
+
+    return newTodo
+}
+
+function getNextId<T extends {id: number}>(items: T[]): number {
+    return items.reduce((max, x) => x.id > max ? x.id : max, 0) + 1
+}
+
+const newTodo = addTodoItem("Buy lots of stuff with all the money we make from the app")
+
+console.log(JSON.stringify(newTodo))
